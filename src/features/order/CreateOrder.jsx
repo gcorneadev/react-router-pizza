0cb90300b59ea09cdd627ba1e16839941f8db462
George Cornea <g.cornea.dev@gmail.com>
2025-12-20 14:10:04 -0800

    Update CreateOrder with action function to provide form data to the route, useActionData and useNavigation hooks provided by react-router-dom to access state and form data

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -33,12 +35,17 @@ const fakeCart = [
 function CreateOrder() {
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
+  // gives access to data recived by route
+  const formErrors = useActionData();
+  // gives access to state
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
 
   return (
     <div>
       <h2>Ready to order? Let's go!</h2>
 
-      <form>
+      <Form method="POST">
         <div>
           <label>First Name</label>
           <input type="text" name="customer" required />
@@ -50,6 +57,8 @@ function CreateOrder() {
             <input type="tel" name="phone" required />
           </div>
         </div>
+
+        {formErrors?.phone && <p>{formErrors.phone}</p>}
 
         <div>
           <label>Address</label>
@@ -70,11 +79,37 @@ function CreateOrder() {
         </div>
 
         <div>
-          <button>Order now</button>
+          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+          <button disabled={isSubmitting}>
+            {isSubmitting ? "Placing order ..." : "Order now"}
+          </button>
         </div>
-      </form>
+      </Form>
     </div>
   );
+}
+
+export async function action({ request }) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  // console.log(data);
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === "on",
+  };
+  // console.log(order);
+
+  const errors = {};
+  if (!isValidPhone(order.phone)) {
+    errors.phone = "Invalid phone number! Please provide a valid phone!";
+  }
+
+  if (Object.keys(errors).length > 0) return errors;
+
+  // create new order if no errors
+  const newOrder = await createOrder(order);
+  return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;

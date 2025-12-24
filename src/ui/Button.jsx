6162fc5ccceeccb287e34f3b2481c67c7ebd3d3f
George Cornea <g.cornea.dev@gmail.com>
2025-12-24 10:22:46 -0800

    Update Loader with css styles. Update index.css with styles for loader class - add theme color.

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom";
+
 export default function Button({ children, ...props }) {
+  const classes =
+    "bg-yellow-400 uppercase font-semibold text-stone-800 inline-block px-4 py-3 tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 hover:cursor-pointer disabled:cursor-not-allowed";
+
+  if (props.to)
+    return (
+      <Link className={classes} {...props}>
+        {children}
+      </Link>
+    );
+
   return (
-    <button
-      {...props}
-      className="bg-yellow-400 uppercase font-semibold text-stone-800 inline-block px-4 py-3 tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 hover:cursor-pointer disabled:cursor-not-allowed"
-    >
+    <button {...props} className={classes}>
       {children}
     </button>
   );
--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,3 +1,8 @@
 export default function Loader() {
-  return <div className="loader"></div>;
+  return (
+    // after color con / add opacity
+    <div className="absolute bg-slate-200/20 inset-0 backdrop-blur-[2px] flex items-center justify-center">
+      <div className="loader"></div>
+    </div>
+  );
 }

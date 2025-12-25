b9ee4333ccff56f57519a7bc7cdf2c1e0db6a3a4
George Cornea <g.cornea.dev@gmail.com>
2025-12-25 09:23:28 -0800

    Update Cart and CartItem with css styles

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import LinkButton from "../../ui/LinkButton";
 import Button from "../../ui/Button";
+import CartItem from "./CartItem";
 
 const fakeCart = [
   {
@@ -30,14 +31,20 @@ function Cart() {
   const cart = fakeCart;
 
   return (
-    <div>
+    <div className="px-4 py-3">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
       {/* <Link className="text-sm text-blue-500 hover:text-blue-600" to="/menu">
         &larr; Back to menu
       </Link> */}
 
-      <h2>Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
+
+      <ul className="divide-y divide-stone-200">
+        {cart.map((item) => (
+          <CartItem item={item} key={item.key}></CartItem>
+        ))}
+      </ul>
 
       <div>
         {/* <Link to="/order/new">Order pizzas</Link> */}

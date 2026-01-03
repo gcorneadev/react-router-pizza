a06f8be742d5afb37d8ff22b6cf3a2b14cf002f5
George Cornea <g.cornea.dev@gmail.com>
2026-01-03 13:50:10 -0800

    Update userSlice with fetchAddress function. Test the function in CreateOrder.

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,10 +2,11 @@ import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import store from "../../store";
 import { clearCart, getTotalPrice } from "../cart/cartSlice";
 import { formatCurrency } from "../../utils/helpers";
+import { fetchAddress } from "../user/userSlice";
 
 
 // https://uibakery.io/regex-library/phone-number
@@ -40,6 +41,8 @@ const fakeCart = [
 
 function CreateOrder() {
 
+  const dispatch = useDispatch();
+
   const username = useSelector((state) => state.user.name);
 
   const [withPriority, setWithPriority] = useState(false);
@@ -55,6 +58,9 @@ function CreateOrder() {
 
   return (
     <div className="px-4 py-6">
+      <div>
+          <Button type="secondary" onClick={() => dispatch(fetchAddress())}>get location</Button>
+        </div>
       <h2 className="font-semibold text-xl mb-6">Ready to order? Let's go!</h2>
 
       <Form method="POST">
@@ -62,6 +68,8 @@ function CreateOrder() {
           <label className="sm:basis-40">First Name</label>
           <input className="input w-full" type="text" name="customer" defaultValue={username} required />
         </div>
+
+        
 
         <div className="mb-4 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Phone number</label>
